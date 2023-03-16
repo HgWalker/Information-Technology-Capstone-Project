adfd246679ab1af878fd8873a0b31b991419344d
HgWalker <98602818+HgWalker@users.noreply.github.com>
2023-03-16 14:54:39 -0400

    Weapons
    Added weapon stats to character js

--- Characters.js
+++ Characters.js
@@ -1,10 +1,23 @@
-let playerCharacter = "";
-let HP;
-let MP;
-let Attack;
-let Defense;
-let Speed;
+//Declarations
+let playerCharacter = ""; //Variable to set character
 
+//Stats
+let HP; //Variable to hold Health Points
+let MP; //Variable to hold Mana Points
+let Attack; //Variable to hold Attack Points
+let Defense; //Variable to hold Defense Points
+let Speed; //Variable to hold Speed Points
+
+//Items
+let weaponName = "";
+let weaponStats;
+let armorName = "";
+let armorStats;
+let amuletName = "";
+let amuletStats;
+
+
+//Functions
 function selectCharacterKai() {
     playerCharacter = "Kai";
     setKai();
