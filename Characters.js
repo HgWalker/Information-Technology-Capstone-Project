8d123852b1c3a2e9cec79a8e8c1575e872d0714c
HgWalker <98602818+HgWalker@users.noreply.github.com>
2023-03-20 10:39:28 -0400

    Characters js page update
    Characters js page now has all of the base stats for all of the characters, weapons, armors, and amulets.

--- Characters.js
+++ Characters.js
@@ -60,3 +60,251 @@ function setTerrin() {
     
 }
 
+//weapons
+function swords() {
+    //wood
+    if(weaponName === "Wooden Sword") {
+        weaponStats = 3;
+    }
+
+    //iron
+    if(weaponName === "Iron Sword") {
+        weaponStats = 6;
+    }
+
+    //Steel
+    if(weaponName === "Steel Sword") {
+        weaponStats = 9;
+    }
+
+    //Royal
+    if(weaponName === "Royal Sword") {
+        weaponStats = 12;
+    }
+
+    //World
+    if(weaponName === "World Sword") {
+        weaponStats = 15;
+    }
+}
+
+function staff() {
+    //Basic
+    if(weaponName === "Basic Staff") {
+        weaponStats = 2;
+    }
+
+    //Enchanted
+    if(weaponName === "Enchanted Staff") {
+        weaponStats = 4;
+    }
+
+    //Amber
+    if(weaponName === "Aquamarine Staff") {
+        weaponStats = 6;
+    }
+
+    //Royal
+    if(weaponName === "Royal Staff") {
+        weaponStats = 8;
+    }
+
+    //World
+    if(weaponName === "World Staff") {
+        weaponStats = 10;
+    }
+}
+
+function Axes() {
+    //Basic
+    if(weaponName === "Hand Axe") {
+        weaponStats = 1;
+    }
+
+    //Enchanted
+    if(weaponName === "Combat Axe") {
+        weaponStats = 2;
+    }
+
+    //Amber
+    if(weaponName === "Great Axe") {
+        weaponStats = 3;
+    }
+
+    //Royal
+    if(weaponName === "Royal Axe") {
+        weaponStats = 4;
+    }
+
+    //World
+    if(weaponName === "World Axe") {
+        weaponStats = 5;
+    }
+}
+
+//Armors
+function chainMail() {
+    //Gold
+    if(armorName === "Gold Mail") {
+        armorStats = 2;
+    }
+
+    //Copper
+    if(armorName === "Copper Mail") {
+        armorStats = 4;
+    }
+
+    //Iron
+    if(armorName === "Iron Mail") {
+        armorStats = 6;
+    }
+
+    //Royal
+    if(armorName === "Mythril Mail") {
+        armorStats = 8;
+    }
+
+    //World
+    if(armorName === "World Mail") {
+        armorStats = 10;
+    }
+}
+
+
+function Robes() {
+    //Silk
+    if(armorName === "Silk Robe") {
+        armorStats = 1;
+    }
+
+    //Leather
+    if(armorName === "Leather Robe") {
+        armorStats = 2;
+    }
+
+    //Enchanted
+    if(armorName === "Enchanted Robe") {
+        armorStats = 3;
+    }
+
+    //Royal
+    if(armorName === "Mythril Robe") {
+        armorStats = 4;
+    }
+
+    //World
+    if(armorName === "World Mail") {
+        armorStats = 5;
+    }
+}
+
+function Plate() {
+    //Stone
+    if(armorName === "Stone Plate") {
+        armorStats = 3;
+    }
+
+    //Bronze
+    if(armorName === "Bronze Plate") {
+        armorStats = 6;
+    }
+
+    //Steel
+    if(armorName === "Steel Plate") {
+        armorStats = 9;
+    }
+
+    //Royal
+    if(armorName === "Mythril Plate") {
+        armorStats = 12;
+    }
+
+    //World
+    if(armorName === "World Plate") {
+        armorStats = 15;
+    }
+}
+
+
+//amulets
+function RedAmulets() {
+    //Agate
+    if(armorName === "Red Agate Amulet") {
+        armorStats = 1;
+    }
+
+    //Garnet
+    if(armorName === "Garnet Amulet") {
+        armorStats = 2;
+    }
+
+    //Ruby
+    if(armorName === "Ruby Amulet") {
+        armorStats = 3;
+    }
+
+    //Red Diamond
+    if(armorName === "Red Diamond Amulet") {
+        armorStats = 4;
+    }
+
+    //World
+    if(armorName === "Red World Amulet") {
+        armorStats = 5;
+    }
+}
+
+function BlueAmulets() {
+    //Azurite
+    if(armorName === "Azurite Amulet") {
+        armorStats = 3;
+    }
+
+    //Lapis Lazuli
+    if(armorName === "Lapis Lazuli Amulet") {
+        armorStats = 6;
+    }
+
+    //Blue Sapphire
+    if(armorName === "Blue Sapphire Amulet") {
+        armorStats = 9;
+    }
+
+    //Blue Diamond
+    if(armorName === "Blue Diamond Amulet") {
+        armorStats = 12;
+    }
+
+    //World
+    if(armorName === "Blue World Amulet") {
+        armorStats = 15;
+    }
+}
+
+function YellowAmulets() {
+    //Agate
+    if(armorName === "Yellow Topaz Amulet") {
+        armorStats = 2;
+    }
+
+    //Garnet
+    if(armorName === "Citrine Amulet") {
+        armorStats = 4;
+    }
+
+    //Ruby
+    if(armorName === "Amber Amulet") {
+        armorStats = 6;
+    }
+
+    //Yellow Diamond
+    if(armorName === "Yellow Diamond Amulet") {
+        armorStats = 8;
+    }
+
+    //World
+    if(armorName === "Yellow World Amulet") {
+        armorStats = 10;
+    }
+}
+
