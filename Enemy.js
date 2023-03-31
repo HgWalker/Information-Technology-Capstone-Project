afb729d5d720cc2a22f6f3a6769832d080296962
HgWalker <98602818+HgWalker@users.noreply.github.com>
2023-03-31 15:51:39 -0400

    week 3 end update
    updated enemies stats section and transferred them to there own enemy js page.

---
+++ Enemy.js
@@ -0,0 +1,115 @@
+
+//Enemy stats
+let slimeHP; //Variable to hold Health Points of the enemy type slime
+let slimeMP; //Variable to hold Mana Points of the enemy type slime
+let slimeAttack; //Variable to hold Attack Points of the enemy type slime
+let slimeDefense; //Variable to hold Defense Points of the enemy type slime
+let slimeSpeed; //Variable to hold Speed Points of the enemy type slime
+
+let greatSlimeHP; //Variable to hold Health Points of the enemy type great slime
+let greatSlimeMP; //Variable to hold Mana Points of the enemy type great slime
+let greatSlimeAttack; //Variable to hold Attack Points of the enemy type great slime
+let greatSlimeDefense; //Variable to hold Defense Points of the enemy type great slime
+let greatSlimeSpeed; //Variable to hold Speed Points of the enemy type great slime
+
+let direWolfHP; //Variable to hold Health Points of the enemy type dire wolf
+let direWolfMP; //Variable to hold Mana Points of the enemy type dire wolf
+let direWolfAttack; //Variable to hold Attack Points of the enemy type dire wolf
+let direWolfDefense; //Variable to hold Defense Points of the enemy type dire wolf
+let direWolfSpeed; //Variable to hold Speed Points of the enemy type dire wolf
+
+let darkElfHP; //Variable to hold Health Points of the enemy type dark elf
+let darkElfMP; //Variable to hold Mana Points of the enemy type dark elf
+let darkElfAttack; //Variable to hold Attack Points of the enemy type dark elf
+let darkElfDefense; //Variable to hold Defense Points of the enemy type dark elf
+let darkElfSpeed; //Variable to hold Speed Points of the enemy type dark elf
+
+//
+let golemHP; //Variable to hold Health Points of the enemy type golem
+let golemMP; //Variable to hold Mana Points of the enemy type golem
+let golemAttack; //Variable to hold Attack Points of the enemy type golem
+let golemDefense; //Variable to hold Defense Points of the enemy type golem
+let golemSpeed; //Variable to hold Speed Points of the enemy type golem
+
+let hydraHP; //Variable to hold Health Points of the enemy type hydra
+let hydraMP; //Variable to hold Mana Points of the enemy type hydra
+let hydraAttack; //Variable to hold Attack Points of the enemy type hydra
+let hydraDefense; //Variable to hold Defense Points of the enemy type hydra
+let hydraSpeed; //Variable to hold Speed Points of the enemy type hydra
+
+let forestElementalHP; //Variable to hold Health Points of the enemy type dark elf
+let forestElementalMP; //Variable to hold Mana Points of the enemy type dark elf
+let forestElementalAttack; //Variable to hold Attack Points of the enemy type dark elf
+let forestElementalDefense; //Variable to hold Defense Points of the enemy type dark elf
+let forestElementalSpeed; //Variable to hold Speed Points of the enemy type dark elf
+
+let griffinHP; //Variable to hold Health Points of the enemy type griffin
+let griffinMP; //Variable to hold Mana Points of the enemy type griffin
+let griffinAttack; //Variable to hold Attack Points of the enemy type griffin
+let griffinDefense; //Variable to hold Defense Points of the enemy type griffin
+let griffinSpeed; //Variable to hold Speed Points of the enemy type griffin
+
+
+//enemy stats
+function setSlime() {
+    slimeHP = 5;
+    slimeMP = 10;
+    slimeAttack = 5;
+    slimeDefense = 8;
+    slimeSpeed = 5;
+    
+}
+
+function setGreatSlime() {
+    greatSlimeHP = 10;
+    greatSlimeMP = 10;
+    greatSlimeAttack = 8;
+    greatSlimeDefense = 10;
+    greatSlimeSpeed = 4;
+    
+}
+
+function setDireWolf() {
+    direWolfHP = 8;
+    direWolfMP = 5;
+    direWolfAttack = 10;
+    direWolfDefense = 5;
+    direWolfSpeed = 10;
+    
+}
+
+function setDarkElf() {
+    darkElfHP = 8;
+    darkElfMP = 10;
+    darkElfAttack = 5;
+    darkElfDefense = 5;
+    darkElfSpeed = 8;
+    
+}
+
+function setForestElemental() {
+    forestElementalHP = 5;
+    forestElementalMP = 8;
+    forestElementalAttack = 10;
+    forestElementalDefense = 10;
+    forestElementalSpeed = 5;
+    
+}
+
+function setHydra() {
+    hydraHP = 10;
+    hydraMP = 10;
+    hydraAttack = 10;
+    hydraDefense = 10;
+    hydraSpeed = 10;
+    
+}
+
+function setGriffin() {
+    griffinHP = 5;
+    griffinMP = 10;
+    griffinAttack = 8;
+    griffinDefense = 5;
+    griffinSpeed = 10;
+
+}
